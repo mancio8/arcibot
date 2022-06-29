def09853f2f2da97277915f642392d7ec8a91ad7
mancio8 <64028211+mancio8@users.noreply.github.com>
2022-06-29 11:24:29 +0200

    commit

--- main.py
+++ main.py
@@ -45,17 +45,102 @@ def invio(update: Update, context: CallbackContext):
         update.message.reply_photo(open("regolamento freccette.jpg", 'rb'))
     if biliardino in update.message.text:
         update.message.reply_photo(open("regolamento BILIARDINO.jpg", 'rb'))
+    if "Gironi calcio balilla umano" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+        #update.message.reply_photo(open("GironeA.jpg", 'rb'))
+        #update.message.reply_photo(open("GironeB.jpg", 'rb'))
+        #update.message.reply_photo(open("GironeC.jpg", 'rb'))
+        #update.message.reply_photo(open("GironeD.jpg", 'rb'))
+    if "Gironi FIFA 22" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+        #update.message.reply_photo(open("FifaA.jpg", 'rb'))
+        #update.message.reply_photo(open("FifaB.jpg", 'rb'))
+        #update.message.reply_photo(open("FifaC.jpg", 'rb'))
+        #update.message.reply_photo(open("FifaD.jpg", 'rb'))
+    if "Gironi Ping Pong" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+        #update.message.reply_photo(open("PingA.jpg", 'rb'))
+        #update.message.reply_photo(open("PingB.jpg", 'rb'))
+        #update.message.reply_photo(open("PingC.jpg", 'rb'))
+        #update.message.reply_photo(open("PingD.jpg", 'rb'))
+    if "Gironi Biliardino" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+        #update.message.reply_photo(open("BilA.jpg", 'rb'))
+        #update.message.reply_photo(open("BilB.jpg", 'rb'))
+        #update.message.reply_photo(open("BilC.jpg", 'rb'))
+        #update.message.reply_photo(open("BilD.jpg", 'rb'))
+    if "Squadre Biliardino" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+    if "Squadre calcio balilla umano" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+    if "Giocatori FIFA 22" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+    if "Giocatori Freccette" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+    if "Giocatori Ping Pong" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+    if "Partite di stasera" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+    if "Risultati partite" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+    if "Prossime partite" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+    if "Classifiche calcio balilla umano" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+        #update.message.reply_photo(open("GironeA.jpg", 'rb'))
+        #update.message.reply_photo(open("GironeB.jpg", 'rb'))
+        #update.message.reply_photo(open("GironeC.jpg", 'rb'))
+        #update.message.reply_photo(open("GironeD.jpg", 'rb'))
+    if "Classifiche FIFA 22" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+        #update.message.reply_photo(open("FifaA.jpg", 'rb'))
+        #update.message.reply_photo(open("FifaB.jpg", 'rb'))
+        #update.message.reply_photo(open("FifaC.jpg", 'rb'))
+        #update.message.reply_photo(open("FifaD.jpg", 'rb'))
+    if "Classifiche Ping Pong" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+        #update.message.reply_photo(open("PingA.jpg", 'rb'))
+        #update.message.reply_photo(open("PingB.jpg", 'rb'))
+        #update.message.reply_photo(open("PingC.jpg", 'rb'))
+        #update.message.reply_photo(open("PingD.jpg", 'rb'))
+    if "Classifiche Biliardino" in update.message.text:
+        update.message.reply_text("Presto disponibili")
+        #update.message.reply_photo(open("BilA.jpg", 'rb'))
+        #update.message.reply_photo(open("BilB.jpg", 'rb'))
+        #update.message.reply_photo(open("BilC.jpg", 'rb'))
+        #update.message.reply_photo(open("BilD.jpg", 'rb'))
+
 
 
 def squadre(update: Update, context: CallbackContext):
-    buttons = [[KeyboardButton("Squadre calcio balilla umano")], [KeyboardButton("Giocatori FIFA 22")], [KeyboardButton("Giocatori Frecette")], [KeyboardButton("Giocatori Ping Pong")], [KeyboardButton("Squadre Biliardino")]]
+    buttons = [[KeyboardButton("Squadre calcio balilla umano")], [KeyboardButton("Giocatori FIFA 22")], [KeyboardButton("Giocatori Freccette")], [KeyboardButton("Giocatori Ping Pong")], [KeyboardButton("Squadre Biliardino")]]
     context.bot.send_message(chat_id=update.effective_chat.id, text="Scegli le squadre che vuoi vedere",
     reply_markup=ReplyKeyboardMarkup(buttons))
-def gironi(update, context):
-    update.message.reply_text("Gironi del calcio balilla umano")
-def partite(update, context):
-    update.message.reply_text("Partite del calcio Balilla umano")
 
+def gironi(update, context):
+    buttons = [[KeyboardButton("Gironi calcio balilla umano")], [KeyboardButton("Gironi FIFA 22")],
+               [KeyboardButton("Gironi Ping Pong")], [KeyboardButton("Gironi Biliardino")]]
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Scegli il torneo di cui vuoi vedere i gironi: ",
+                             reply_markup=ReplyKeyboardMarkup(buttons))
+def partite(update, context):
+    buttons = [[KeyboardButton("Partite di stasera")], [KeyboardButton("Risultati partite")],
+               [KeyboardButton("Prossime partite")]]
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Scegli le partite che ti interessano: ",
+                             reply_markup=ReplyKeyboardMarkup(buttons))
+
+def classifiche(update, context):
+    buttons = [[KeyboardButton("Classifiche calcio balilla umano")], [KeyboardButton("Classifiche FIFA 22")],
+               [KeyboardButton("Classifiche Ping Pong")], [KeyboardButton("Classifiche Biliardino")]]
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Scegli la classifica che vuoi visualizzare: ",
+                             reply_markup=ReplyKeyboardMarkup(buttons))
+
+
+def premi(update, context):
+    update.message.reply_photo(open("PremiCB.jpg", 'rb'))
+    update.message.reply_photo(open("PremiFr.jpg", 'rb'))
+    update.message.reply_photo(open("PremioB.jpg", 'rb'))
+    update.message.reply_photo(open("PremioPP.jpg", 'rb'))
+    update.message.reply_photo(open("PremioF.jpg", 'rb'))
 
 
 updater = Updater(TOKEN, use_context=True)
@@ -64,5 +149,7 @@ updater.dispatcher.add_handler(CommandHandler('regole', regole))
 updater.dispatcher.add_handler(CommandHandler('squadre', squadre))
 updater.dispatcher.add_handler(CommandHandler('gironi', gironi))
 updater.dispatcher.add_handler(CommandHandler('partite', partite))
+updater.dispatcher.add_handler(CommandHandler('classifiche', classifiche))
+updater.dispatcher.add_handler(CommandHandler('premi', premi))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, invio))
 updater.start_polling()
